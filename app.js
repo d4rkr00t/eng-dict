d0dc14bf9621694c5cc6a7cf84d9701a03bec4d0
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-09-20 17:56:24 +0300

    Init setup

---
+++ app.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console, no-process-exit */
+import Application from './modules/application';
+import parseConfig from './modules/config';
+
+const basePath = __dirname;
+const appConfig = parseConfig(basePath);
+const application = new Application(appConfig, basePath);
+
+// `catch` only needed to catch errors during application startup
+application
+  .execute()
+  .catch(function (error) {
+    console.error(error.stack ? error.stack : error);
+    process.exit(1);
+  });
+
+process.on('exit', function () {
+  application.shutdown();
+});
